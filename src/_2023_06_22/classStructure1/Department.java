e081c50b298a9844b411a56320d926429acf2880
RLedenev <led.rom.93@gmail.com>
2023-06-22 18:01:03 +0200

    commit

---
+++ src/_2023_06_22/classStructure1/Department.java
@@ -0,0 +1,11 @@
+package _2023_06_22.classStructure1;
+
+public enum Department {
+    GENERAL_MANAGEMENT,
+    MARKETING,
+    OPERATIONS,
+    FINANCE,
+    SALES,
+    HUMAN_RESOURCE,
+    PURCHASE
+}
---
+++ src/_2023_06_22/classStructure1/Employee.java
@@ -0,0 +1,47 @@
+package _2023_06_22.classStructure1;
+
+public class Employee {
+    private String name;
+    private Department department;
+    private Position position;
+    private double salary;
+
+    public Employee(String name, Department department, Position position, double salary) {
+        this.name = name;
+        this.department = department;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
